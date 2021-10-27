71276a859cf35637edee4e3d05c8ed29de9d05bd
Soo <DSsooh578@likelion.org>
2021-10-27 17:14:12 +0900

    delecte secrete key2

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,11 +44,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
 #SECRET_KEY = ''
-=======
-
->>>>>>> d87fba0a15b36be1886850e643d7f226c5ccbb7b
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
